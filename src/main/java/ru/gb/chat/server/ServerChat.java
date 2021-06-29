b63967acb478990f3c4aa596f7baab3b89bffe74
Andrej <andrej31.10.92@rambler.ru>
2021-06-29 10:12:53 +0300

    Lesson6

--- src/main/java/ru/gb/chat/server/ServerChat.java
+++ src/main/java/ru/gb/chat/server/ServerChat.java
@@ -1,26 +1,44 @@
 package ru.gb.chat.server;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * Created by Artem Kropotov on 17.05.2021
  */
 public class ServerChat {
+    private static final Logger logger = Logger.getLogger("admin");
     private final CopyOnWriteArrayList<ClientHandler> clients = new CopyOnWriteArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         new ServerChat().start();
     }
 
-    public void start() {
+    public void start() throws UnsupportedEncodingException {
+        FileHandler fileHandler = null;
+        try {
+            fileHandler = new FileHandler("MyLOG.log");
+            fileHandler.setEncoding("UTF-8");
+            fileHandler.setFormatter(new SimpleFormatter());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logger.addHandler(fileHandler);
         try(ServerSocket serverSocket = new ServerSocket(8189)) {
-            System.out.println("Сервер запущен");
+//            System.out.println("Сервер запущен");
+            logger.info("Сервер запущен");
             while (true) {
                 Socket socket = serverSocket.accept();
-                System.out.println("Клиент подключился");
+//                System.out.println("Клиент подключился");
+                logger.info("Клиент подключился");
                 new ClientHandler(socket, this);
             }
         } catch (IOException e) {
@@ -59,16 +77,20 @@ public class ServerChat {
     public void privateMsg(ClientHandler sender, String nick, String message) {
         if (sender.getUser().getNickname().equals(nick)) {
             sender.sendMessage("Заметка для себя: " + message);
+            logger.info(sender.getUser().getNickname() + "Заметка для себя: " + message);
         }
 
         for (ClientHandler receiver : clients) {
             if (receiver.getUser().getNickname().equals(nick)) {
                 receiver.sendMessage("от " + sender.getUser().getNickname() + ": " + message);
                 sender.sendMessage("для " + nick + ": " + message);
+                logger.info(sender.getUser().getNickname() + "Написал сообщение :" + message + " для : " + nick);
+
                 return;
             }
         }
         sender.sendMessage("Клиент " + nick + " не найден");
+        logger.info("Клиент " + nick + " не найден");
     }
 
     public void broadcastClientList() {
